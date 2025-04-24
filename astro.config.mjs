34d93741607893578964f2133c437e98c602eefb
Ingrid1802 <ingridaybar841@gmail.com>
2025-04-24 12:26:25 -0600

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://github.com/Ingrid1802/portfolio-web',
+  outDir: 'dist',
   output: "server",
   vite: {
     plugins: [tailwindcss()],
